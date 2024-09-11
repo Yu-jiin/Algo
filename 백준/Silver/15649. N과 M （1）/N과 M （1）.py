ad09febb309bdfd9dee9122916812a009c637416
Yu-jiin <155607382+Yu-jiin@users.noreply.github.com>
2024-09-11 14:43:46 +0900

    [Silver III] Title: N과 M (1), Time: 172 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,19 @@
+N, M = map(int, input().split())
+arr = [i for i in range(1, N+1)]
+visited = [0] * M
+
+
+def backtracking(level):
+    if level == M:
+        print(*visited)
+        return
+
+    for i in range(N):
+        if arr[i] in visited:
+            continue
+
+        visited[level] = arr[i]
+        backtracking(level + 1)
+        visited[level] = 0
+
+backtracking(0)
